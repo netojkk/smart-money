56da6154709ed58e56ee40cb0793d92bc7923772
Neto <136371812+netojkk@users.noreply.github.com>
2025-06-15 14:13:50 -0300

    feat: first commit

---
+++ src/main/java/com/neto/smart_money/SmartMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.neto.smart_money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neto/smart_money/SmartMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neto.smart_money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
