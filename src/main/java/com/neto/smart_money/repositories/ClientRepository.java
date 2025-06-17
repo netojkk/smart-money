7ed74c5483f017afbce3cc08185604ce38786dae
Neto <136371812+netojkk@users.noreply.github.com>
2025-06-17 10:32:31 -0300

    feat: rename of user for client

--- src/main/java/com/neto/smart_money/repositories/ClientRepository.java
+++ src/main/java/com/neto/smart_money/repositories/ClientRepository.java
@@ -1,9 +1,9 @@
 package com.neto.smart_money.repositories;
 
-import com.neto.smart_money.domain.entities.user.User;
+import com.neto.smart_money.domain.entities.client.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface ClientRepository extends JpaRepository<Client, UUID> {
 }
