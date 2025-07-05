b923567da4ceea10d212689629f8ec914bd5ea3e
Neto <136371812+netojkk@users.noreply.github.com>
2025-07-05 17:34:16 -0300

    feat: atualization

--- src/main/java/com/neto/smart_money/domain/entities/lauch/Launch.java
+++ src/main/java/com/neto/smart_money/domain/entities/lauch/Launch.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor@AllArgsConstructor
-public class  Launch {
+public class   Launch {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
