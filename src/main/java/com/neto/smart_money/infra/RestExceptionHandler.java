4c040f51606df2cc030aa53695592f1b63e20488
Neto <136371812+netojkk@users.noreply.github.com>
2025-07-05 17:34:36 -0300

    feat: creating the main handler class

--- src/main/java/com/neto/smart_money/infra/RestExceptionHandler.java
+++ src/main/java/com/neto/smart_money/infra/RestExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.neto.smart_money.infra;
 
-import com.neto.smart_money.exceptions.EmailDuplicateException;
-import com.neto.smart_money.exceptions.UserNotFoundException;
-import com.neto.smart_money.exceptions.WrongPasswordException;
+import com.neto.smart_money.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -29,5 +26,29 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<ApiErrorMessage> wrongPasswordHandler(WrongPasswordException e){
         ApiErrorMessage threatResponse = new ApiErrorMessage(HttpStatus.UNAUTHORIZED, e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(threatResponse);
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    private ResponseEntity<ApiErrorMessage> accountNotFoundHandler(AccountNotFoundException e){
+        ApiErrorMessage threatResponse = new ApiErrorMessage(HttpStatus.NOT_FOUND, e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(threatResponse);
+    }
+
+    @ExceptionHandler(InvalidTransactionAmountException.class)
+    private ResponseEntity<ApiErrorMessage> invalidTransactionAmountHandler(InvalidTransactionAmountException e){
+        ApiErrorMessage threatResponse = new ApiErrorMessage(HttpStatus.BAD_REQUEST, e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(threatResponse);
+    }
+
+    @ExceptionHandler(CategoryDuplicateException.class)
+    private ResponseEntity<ApiErrorMessage> categoryDuplicateHandler(CategoryDuplicateException e){
+        ApiErrorMessage threatResponse = new ApiErrorMessage(HttpStatus.CONFLICT, e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(threatResponse);
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    private ResponseEntity<ApiErrorMessage> categoryNotFoundHandler(CategoryNotFoundException e){
+        ApiErrorMessage threatResponse = new ApiErrorMessage(HttpStatus.NOT_FOUND, e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(threatResponse);
     }
 }
