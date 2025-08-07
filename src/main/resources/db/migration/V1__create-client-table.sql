f5e2fe600a5077bd4bd5b0aaacd69572d8e944b0
Jose Francisco <francesneto6@gmail.com>
2025-08-07 12:25:23 -0300

    fix: adding the column "role" to database

--- src/main/resources/db/migration/V1__create-client-table.sql
+++ src/main/resources/db/migration/V1__create-client-table.sql
@@ -4,5 +4,6 @@ CREATE TABLE client(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100)UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    role TEXT NOT NULL
 );
