9937e27c03633900a3415422ea482317ab17dcc9
Jose Francisco <francesneto6@gmail.com>
2025-08-16 04:46:34 -0300

    feat: new methods to checking on AccountRepository

--- src/main/java/com/neto/smart_money/repositories/AccountRepository.java
+++ src/main/java/com/neto/smart_money/repositories/AccountRepository.java
@@ -5,9 +5,13 @@ import com.neto.smart_money.domain.entities.client.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, UUID> {
 
     List<Account> findByClientId(UUID clientId);
+    Optional<Account> findByIdAndClientId(UUID AccountId , UUID clientId);
+    Optional<Account> findByNameAndClientId(String AccountName, UUID clientId);
+
 }
