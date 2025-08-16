cbfb6604711810866572c7b7ca30855fee026645
Jose Francisco <francesneto6@gmail.com>
2025-08-16 04:51:49 -0300

    fix: removing the clientId of AccountRequestDTO

--- src/main/java/com/neto/smart_money/dto/AccountRequestDTO.java
+++ src/main/java/com/neto/smart_money/dto/AccountRequestDTO.java
@@ -3,5 +3,5 @@ package com.neto.smart_money.dto;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record AccountRequestDTO (String name, BigDecimal balance, UUID clientId) {
+public record AccountRequestDTO (String name, BigDecimal balance) {
 }
