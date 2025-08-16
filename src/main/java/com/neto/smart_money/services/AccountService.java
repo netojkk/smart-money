02d1c379e1b6f778b16bca42fbd29cb6ee07ddaa
Jose Francisco <francesneto6@gmail.com>
2025-08-16 04:43:50 -0300

    feat: adding the authentication by JWT AccountService

--- src/main/java/com/neto/smart_money/services/AccountService.java
+++ src/main/java/com/neto/smart_money/services/AccountService.java
@@ -4,18 +4,17 @@ import com.neto.smart_money.domain.entities.account.Account;
 import com.neto.smart_money.domain.entities.client.Client;
 import com.neto.smart_money.dto.AccountRequestDTO;
 import com.neto.smart_money.dto.AccountResponseDTO;
-import com.neto.smart_money.exceptions.custom.AccountNotFoundException;
-import com.neto.smart_money.exceptions.custom.DifferentUserException;
-import com.neto.smart_money.exceptions.custom.InvalidTransactionAmountException;
-import com.neto.smart_money.exceptions.custom.UserNotFoundException;
+import com.neto.smart_money.exceptions.custom.*;
 import com.neto.smart_money.repositories.AccountRepository;
 import com.neto.smart_money.repositories.ClientRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -25,8 +24,22 @@ public class AccountService {
     private ClientRepository clientRepository;
     private AccountRepository accountRepository;
 
+    private Client getAuthenticatedClient() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return clientRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User Not Found"));
+    }
+
     public AccountResponseDTO createAccount(AccountRequestDTO data){
-        Client client = this.clientRepository.findById(data.clientId()).orElseThrow(() -> new UserNotFoundException("User Not Found!"));
+        Client client = getAuthenticatedClient();
+
+        Optional<Account> search = this.accountRepository.findByNameAndClientId(data.name(), client.getId());
+
+        if (search.isPresent()){
+
+            throw new AccountAlreadyExistsException("Account Already exists");
+
+        }
 
         Account account = new Account();
         account.setName(data.name());
@@ -37,18 +50,26 @@ public class AccountService {
         return new AccountResponseDTO(saved.getId(), saved.getName(), saved.getBalance());
     }
 
-    public List<AccountResponseDTO> getAllByClient(UUID clientId){
-        return accountRepository.findByClientId(clientId).stream()
+    public List<AccountResponseDTO> getAllByClient(){
+        Client client = getAuthenticatedClient();
+
+        return accountRepository.findByClientId(client.getId()).stream()
                 .map(acc -> new AccountResponseDTO(acc.getId(), acc.getName(), acc.getBalance()))
                         .toList();
 
     }
 
+    @Transactional
     public AccountResponseDTO deposit(UUID accountId, BigDecimal amount){
+        Client client = getAuthenticatedClient();
+
+        Account account = this.accountRepository.findByIdAndClientId(accountId,client.getId())
+                .orElseThrow(() -> new AccountNotFoundException("Account Not Found"));
+
         if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0 ){
             throw new InvalidTransactionAmountException("The value of the transaction must be greater than zero.");
         }
-        Account account = this.accountRepository.findById(accountId).orElseThrow(() -> new AccountNotFoundException("Account Not Found"));
+
 
         account.setBalance(account.getBalance().add(amount));
         Account updated = accountRepository.save(account);
@@ -56,13 +77,16 @@ public class AccountService {
 
     }
 
+    @Transactional
     public AccountResponseDTO withdraw(UUID accountId, BigDecimal amount){
+        Client client = getAuthenticatedClient();
+
+        Account account = this.accountRepository.findByIdAndClientId(accountId,client.getId())
+                .orElseThrow(() -> new AccountNotFoundException("Account Not Found"));
+
         if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0){
             throw new InvalidTransactionAmountException("The value of transaction must be greater than zero.");
         }
-
-        Account account = this.accountRepository.findById(accountId).orElseThrow(() -> new AccountNotFoundException("Account Not Found"));
-
         if (account.getBalance().compareTo(amount) < 0){
             throw new InvalidTransactionAmountException("Insufficient balance for withdrawal.");
         }
@@ -71,13 +95,14 @@ public class AccountService {
         Account updated = accountRepository.save(account);
         return new AccountResponseDTO(updated.getId(), updated.getName(), updated.getBalance());
     }
-    @Transactional
-    public void deleteById(UUID clientId,UUID accountId){
-        Account account = this.accountRepository.findById(accountId).orElseThrow( () -> new AccountNotFoundException("Account Not Found"));
 
-        if(!account.getClient().getId().equals(clientId)){
-            throw new DifferentUserException("That account doesn't exists");
-        }
+    @Transactional
+    public void deleteById(UUID accountId){
+        Client client = getAuthenticatedClient();
+
+        Account account = this.accountRepository.findByIdAndClientId(accountId, client.getId()).orElseThrow(
+                () -> new AccountNotFoundException("Account Not Found"));
+
 
         accountRepository.deleteById(account.getId());
     }
