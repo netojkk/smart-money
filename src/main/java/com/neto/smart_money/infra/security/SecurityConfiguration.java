3899433311375a3c12fbf999d8f69356031d541e
Jose Francisco <francesneto6@gmail.com>
2025-08-16 04:41:17 -0300

    fix: adding the SecurityFilter

--- src/main/java/com/neto/smart_money/infra/security/SecurityConfiguration.java
+++ src/main/java/com/neto/smart_money/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.neto.smart_money.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity //this line allows the setting of web security
 public class SecurityConfiguration {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean //this creating the filters to security. So that i do it my self
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -25,7 +30,8 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/clients").hasRole("ADMIN")
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()
+                        ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                         .build();
     }
 
