6e6b229ebbac209a1213e752012edf7b9fb71199
Jose Francisco <francesneto6@gmail.com>
2025-08-16 04:57:48 -0300

    feat: LaunchRepository with methods

--- src/main/java/com/neto/smart_money/repositories/LaunchRepository.java
+++ src/main/java/com/neto/smart_money/repositories/LaunchRepository.java
@@ -3,7 +3,19 @@ package com.neto.smart_money.repositories;
 import com.neto.smart_money.domain.entities.lauch.Launch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LaunchRepository extends JpaRepository<Launch, UUID> {
+    boolean existsByAccountIdAndDateAndDescriptionAndAmount(
+            UUID accountId,
+            LocalDate transactionDate,
+            String description,
+            BigDecimal amount
+    );
+
+    List<Launch> findByClientId(UUID clientId);
 }
